12c39d2dd9af227991e81952e1e54f793e1b9eb0
Matskrane <69842790+Matskrane@users.noreply.github.com>
2022-06-09 02:24:50 +0200

    xd

--- src/App.js
+++ src/App.js
@@ -4,38 +4,41 @@ import Home from "./pages/Home";
 import SideBar from "./components/SideBar";
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Switch from 'react-js-switch';
-import { createContext, useState } from "react";
+import { FormControlLabel } from '@mui/material';
+import { useState } from "react";
+import { MaterialUISwitch } from './components/StyledThemeSwitcher';
 
-export const ThemeContext = createContext(null);
+
 
 function App() {
 
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    setTheme((current) => (current === "light" ? "dark" : "light"));
   };
 
   return (
     <>
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
-        <div className="main-container" id={theme}>
-          <Router>
-            <SideBar />
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </Router>
-          <div className="switch">
-            <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-            <Switch onChange={toggleTheme} value={theme === "dark"} />
-          </div>
+      <div className="main-container" id={theme}>
+        <Router>
+          <SideBar />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+        <div className="switch">
+          <label className='label'> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <FormControlLabel
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+            control={<MaterialUISwitch sx={{ m: 1 }} />}
+          />
         </div>
-      </ThemeContext.Provider>
+      </div>
     </>
   );
 }
