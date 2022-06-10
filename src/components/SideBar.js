ca1270b4b6de34bb240ee6c987957cba6e519878
Matskrane <69842790+Matskrane@users.noreply.github.com>
2022-06-10 23:29:17 +0200

    xd

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,6 @@
 import { faBriefcase, faComments, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
 
@@ -9,24 +10,33 @@ const SideBar = () => {
         return {
             color: isActive  
         }
-    }
+    };
+    
+    const [toggle, setToggle] = useState(true);
     
   return (
-    <>
+    <>  
+
+<div onClick={() => setToggle(!toggle)} className='nav-toggler'>
+            <span></span>
+        </div>
+        {toggle && (
         <div className='aside'>
             <div className='logo'>
                 <Link to={'/'}><span>𝗠</span>ats</Link>
             </div>
-            <div className='nav-toggler'>
-                <span></span>
-            </div>
+
+
+            
             <ul className='nav'>
                 <li><NavLink style={currentActiveNavLink} to={'/'}><FontAwesomeIcon  icon={faHouse} />Home</NavLink></li>
                 <li><NavLink style={currentActiveNavLink} to={'/about'}><FontAwesomeIcon  icon={faUser} />About</NavLink></li>
                 <li><NavLink style={currentActiveNavLink} to={'/projects'}><FontAwesomeIcon icon={faBriefcase} />Projects</NavLink></li>
                 <li><NavLink style={currentActiveNavLink} to={'/contact'}><FontAwesomeIcon icon={faComments} />Contact</NavLink></li>
             </ul>
+            
         </div>
+        )}
     </>
   )
 }
