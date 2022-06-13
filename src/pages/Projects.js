978c76ef2660b0c793818b0f87ced6c59f83f6a8
Matskrane <69842790+Matskrane@users.noreply.github.com>
2022-06-14 01:08:09 +0200

    project cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,6 +21,33 @@ const Projects = () => {
               <div className="portfolio-item-inner shadow-dark">
                 <div className="portfolio-img">
                   <img src="/images/holidaze.png" alt="" />
+                  <div className="project-info padd-15">
+                  <h4>Holidaze - Hotel Booking</h4>  
+                  <div className="stack">
+                    <div className="stack-description">
+                      <h5>Description:</h5>
+                      <ul>
+                        <li>Make a Hotel booking website located in Bergen</li>
+                        <li>An Admin page using jwt tokens.</li>
+                        <li>Using strapi api as backend for the project connected to Heroku.</li>
+                      </ul>
+                    </div>
+                    <div className="stack-used">
+                      <h5>Built With:</h5>
+                      <ul>
+                        <li>NextJS</li>
+                        <li>Boostrap</li>
+                        <li>Sass</li>
+                        <li>Heroku</li>
+                        <li>Strapi</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="project-links">
+                    <a>Github</a>
+                    <a>Live Demo</a>
+                  </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -30,6 +57,33 @@ const Projects = () => {
               <div className="portfolio-item-inner shadow-dark">
                 <div className="portfolio-img">                 
                   <img src="/images/strapi-project (13).png" alt="" />
+                  <div className="project-info padd-15">
+                  <h4>Holidaze - Hotel Booking</h4>  
+                  <div className="stack">
+                    <div className="stack-description">
+                      <h5>Description:</h5>
+                      <ul>
+                        <li>Make a Hotel booking website located in Bergen</li>
+                        <li>An Admin page using jwt tokens.</li>
+                        <li>Using strapi api as backend for the project connected to Heroku.</li>
+                      </ul>
+                    </div>
+                    <div className="stack-used">
+                      <h5>Built With:</h5>
+                      <ul>
+                        <li>NextJS</li>
+                        <li>Boostrap</li>
+                        <li>Sass</li>
+                        <li>Heroku</li>
+                        <li>Strapi</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="project-links">
+                    <a>Github</a>
+                    <a>Live Demo</a>
+                  </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -39,6 +93,33 @@ const Projects = () => {
               <div className="portfolio-item-inner shadow-dark">
                 <div className="portfolio-img">
                   <img src="/images/blog.png" alt="" />
+                  <div className="project-info padd-15">
+                  <h4>Holidaze - Hotel Booking</h4>  
+                  <div className="stack">
+                    <div className="stack-description">
+                      <h5>Description:</h5>
+                      <ul>
+                        <li>Make a Hotel booking website located in Bergen</li>
+                        <li>An Admin page using jwt tokens.</li>
+                        <li>Using strapi api as backend for the project connected to Heroku.</li>
+                      </ul>
+                    </div>
+                    <div className="stack-used">
+                      <h5>Built With:</h5>
+                      <ul>
+                        <li>NextJS</li>
+                        <li>Boostrap</li>
+                        <li>Sass</li>
+                        <li>Heroku</li>
+                        <li>Strapi</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="project-links">
+                    <a>Github</a>
+                    <a>Live Demo</a>
+                  </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -48,6 +129,33 @@ const Projects = () => {
               <div className="portfolio-item-inner shadow-dark">
                 <div className="portfolio-img">
                   <img src="/images/bootstrap-project.png" alt="" />
+                  <div className="project-info padd-15">
+                  <h4>Holidaze - Hotel Booking</h4>  
+                  <div className="stack">
+                    <div className="stack-description">
+                      <h5>Description:</h5>
+                      <ul>
+                        <li>Make a Hotel booking website located in Bergen</li>
+                        <li>An Admin page using jwt tokens.</li>
+                        <li>Using strapi api as backend for the project connected to Heroku.</li>
+                      </ul>
+                    </div>
+                    <div className="stack-used">
+                      <h5>Built With:</h5>
+                      <ul>
+                        <li>NextJS</li>
+                        <li>Boostrap</li>
+                        <li>Sass</li>
+                        <li>Heroku</li>
+                        <li>Strapi</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="project-links">
+                    <a>Github</a>
+                    <a>Live Demo</a>
+                  </div>
+                  </div>
                 </div>
               </div>
             </div>
